77c8d7fb75650a43667c775138a208d46375ca6a
Reeves Smith <reeves.smith@macerconsulting.com>
2021-04-07 12:36:40 -0600

    update to individual models

--- dbt-learn/models/customer_orders.sql
+++ dbt-learn/models/customer_orders.sql
@@ -1,37 +1,38 @@
-with customers as (
+with 
+customers as 
+(
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select *
+    from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * 
+    from {{ ref('stg_orders') }}
 
 ),
 
+payments as (
+
+    select * 
+    from {{ ref('stg_payments') }}
+    where STATUS = 'success'
+),
+
+
+
 customer_orders as (
 
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
+    select orders.customer_id
+         , min(orders.order_date) as first_order_date
+         , max(orders.order_date) as most_recent_order_date
+         , count(orders.order_id) as number_of_orders
+         , SUM(payments.AMOUNT) as lifetime_ammount
+    from orders 
+    left
+    join payments ON orders.order_id = payments.order_id 
     group by 1
 
 ),
@@ -45,11 +46,10 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_ammount, 0) as lifetime_ammount
     from customers
-
-    left join customer_orders using (customer_id)
+    left join customer_orders ON customers.customer_id = customer_orders.customer_id
 
 )
 
